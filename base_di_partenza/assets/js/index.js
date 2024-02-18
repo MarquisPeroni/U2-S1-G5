bb37d374c4f78e873d87b6615be8dc6ff790c815
MarquisPeroni <marco_p2009@live.it>
2024-02-18 17:31:38 +0100

    fifthcommit.adding.js

--- base_di_partenza/assets/js/index.js
+++ base_di_partenza/assets/js/index.js
@@ -3,13 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("scroll", function() {
         let scrollPosition = window.scrollY;
+        let inputButton = document.getElementById("blackstart");
+        
 
         if (scrollPosition > 80) {
             header.classList.remove("yellow");
             header.classList.add("white");
+            inputButton.style.backgroundColor ="#1a8917";
         } else {
             header.classList.remove("white");
             header.classList.add("yellow");
+            inputButton.style.backgroundColor ="black";
         }
     });
 });
